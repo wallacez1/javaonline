ff719b2e9525c61ad1af3debe7265ec60d5acb83
wallace <wallace_silva@unigranrio.br>
2018-03-05 23:14:34 -0300

    Projeto java

--- src/br/com/abc/javacore/Fmodificadorestatico/classes/Cliente.java
+++ src/br/com/abc/javacore/Fmodificadorestatico/classes/Cliente.java
@@ -2,7 +2,7 @@ package br.com.abc.javacore.Fmodificadorestatico.classes;
 
 public class Cliente {
 
-
+    // 0 - Bloco de inicialização é executado quando a jvm carregar a classe (é executado apenas 1 vez)
     // 1 - Alocado espaço na memoria para objeto criado
     // 2 - Cada atributo de classe é criado e inicializado com seus valores default ou explicitos
     // 3 - Bloco de inicialização é executado
